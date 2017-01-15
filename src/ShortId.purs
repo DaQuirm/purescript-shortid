94fe7628db0a122f496a84956ea245e73ff6e625
DaQuirm <leo.daquirm@gmail.com>
2017-01-15 12:19:41 +0100

    Fix type signature

--- src/ShortId.purs
+++ src/ShortId.purs
@@ -4,4 +4,4 @@ import Control.Monad.Eff (Eff)
 
 foreign import data SHORTID :: !
 
-foreign import generate :: forall e. Eff (random :: SHORTID | e) String
+foreign import generate :: forall e. Eff (shortid :: SHORTID | e) String
